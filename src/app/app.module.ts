83038f8ffde22e2b82591e80201719f114469583
José Manuel García Fernández <aleajactaesgf@gmail.com>
2020-02-05 10:09:07 +0100

    Primer commit App SIN effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+// RUTAS
+import { AppRoutingModule } from './app-routing.module';
+
+// MODULOS PERSONALIZADOS
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    UsuariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,21 @@
+
+export class Usuario {
+    public id: number;
+    public email: string;
+    // tslint:disable-next-line: variable-name
+    public first_name: string;
+    // tslint:disable-next-line: variable-name
+    public last_name: string;
+    public avatar: string;
+    // constructor(
+    //     id: number,
+    //     email: string,
+    //     // tslint:disable-next-line: variable-name
+    //     first_name: string,
+    //     // tslint:disable-next-line: variable-name
+    //     last_name: string,
+    //     avatar: string
+    // ) { }
+}
+
+
